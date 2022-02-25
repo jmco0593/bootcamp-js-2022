33ac66d054b1be85706ed555c2030f408cbb6abb
jmco_0593 <jorgemario.cardonaoliva@gmail.com>
2022-02-24 22:10:43 -0600

    Se pusieron CONST

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 //Permite obtener los elementos con el tag p
-let paragraphs = document.getElementsByTagName("p");
-console.log(paragraphs);
+const paragraphs = document.getElementsByTagName("p");
+console.log("Parrafos en el documento:", paragraphs);
 
 // el resultante es un arreglo, por lo que puedo tratarlo como tal
 if (paragraphs.length > 0)
 {
-    let paragraph = paragraphs[0];
+    const paragraph = paragraphs[0];
     //Con innerText puedo cambiar textos de algun elemento
     paragraph.innerText = "Bienvenidos al Bootcamp";
 }
