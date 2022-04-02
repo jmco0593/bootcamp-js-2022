553835789b122881d76640e1c7a732fce5b0837f
jmco_0593 <jorgemario.cardonaoliva@gmail.com>
2022-04-02 12:35:01 -0600

    Reto 11 v2

--- retos/11-happyNumber.js
+++ retos/11-happyNumber.js
@@ -4,22 +4,27 @@ function happyNumber(number)
     const iteration_limit = 1000;
     let i = 0;
     let total = 0;
+    let repeated_numbers = [];
     while(i <= iteration_limit)
     {
         total = 0;
         for(n of number.toString())
         {
             total = total + Math.pow(Number(n), 2);
-            console.log()
         }
         if (total == 1)
         {
             res = true;
             break;
         }
+        if (repeated_numbers.includes(total)){break;}
+        repeated_numbers.push(total);
         number = total;
         i++;
 
     }
     return res;
 }
+
+const test = 97;
+console.log(happyNumber(test));
