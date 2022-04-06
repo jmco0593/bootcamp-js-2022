483d886bd20c39508f120bc3bb65a6f7c7cc844e
jmco_0593 <jorgemario.cardonaoliva@gmail.com>
2022-04-06 09:42:27 -0600

    Proyect improve

--- proyecto/js/js/index.js
+++ proyecto/js/js/index.js
@@ -1,11 +1,14 @@
+
 //DEFINITIONS
 const taskInput = document.getElementById("taskInput");
 const tasksList = document.getElementById("taskList");
+const completedTaskList = document.getElementById("completedTaskList");
 const addTaskButton = document.getElementById("addTaskButton");
 const clearTasksButton = document.getElementById("clearTasksButton");
 const counterSpan = document.getElementById("counterSpan");
 const body = document.getElementsByTagName("body")[0];
 var taskCounter = 0;
+var completedTaskCounter = 0;
 
 body.addEventListener("keyup", function(event) {
     if (event.keyCode === 13) {
@@ -16,29 +19,57 @@ body.addEventListener("keyup", function(event) {
 
 taskInput.focus();
 
+
+function createTask(txt)
+{
+    const task = document.createElement("li");
+    var text = document.createTextNode(txt);
+    task.setAttribute('class', 'task');
+    task.setAttribute('onClick', 'editTask()');
+    task.appendChild(text);
+    return task;
+}
+
 function addTask()  
 {
     taskInput.focus();
     if (taskInput.value)
     {
-        const taskText = taskInput.value; 
-        const task = document.createElement("li");
-        var text = document.createTextNode(taskText);
-        task.setAttribute('class', 'task');
-        task.setAttribute('onClick', 'editTask()');
-        task.setAttribute('contenteditable', 'true');
-        task.innerHTML = `
-            <span onclick="removeTask(event)"><i class="bi bi-trash"></i></span>
+        const task = createTask(taskInput.value);
+        task.innerHTML = task.innerHTML + `
+            <span class="check" onclick="checkTask(event)"><i class="bi bi-check2-square"></i></span><span class="delete" onclick="removeTask(event)"><i class="bi bi-trash"></i></span>
         `
         tasksList.appendChild(task);
-        task.appendChild(text);
         taskInput.value = "";
-        updateCounter("up")
+        updateTaskCounter("up")
     }
     else
     {
         alert("Please enter some text in task input");
     }
+}
+
+function checkTask(event)
+{
+    taskInput.focus();
+    const anchor = event.currentTarget;
+    const li = anchor.parentElement;
+    const txt = li.textContent;
+    task = createTask(txt);
+
+    completedTaskList.appendChild(task);
+    removeTask(event);
+    updateTaskCounter("down");
+}
+
+function removeTask(event)
+{
+    console.log("remover");
+    const anchor = event.currentTarget;
+    const li = anchor.parentElement;
+    tasksList.removeChild(li);
+    taskInput.focus();  
+    updateTaskCounter("down");   
 }
 
 function clearList()
@@ -49,16 +80,24 @@ function clearList()
         {
             removeChilds(tasksList);
             taskInput.focus();
-            updateCounter("clear");
+            updateTaskCounter("clear");
         }
     }
     else
     {
         alert("Please add some task first");
     }
-    
-    
 }
+
+function updateTaskCounter(val)
+{
+    if (val == "up"){taskCounter = taskCounter + 1;}
+    else if (val == 'down'){taskCounter = taskCounter - 1;}
+    else if("clear"){taskCounter = 0;}
+    counterSpan.textContent = taskCounter;
+}
+
+
 
 function removeChilds(parent)
 {
@@ -70,36 +109,11 @@ function removeChilds(parent)
 }
 
 
-function removeTask(event)
+function updateCompletedTaskCounter(val)
 {
-    console.log("remover");
-    const anchor = event.currentTarget;
-    const li = anchor.parentElement;
-    tasksList.removeChild(li);
-    taskInput.focus();
-    updateCounter("down");   
-}
-
-function updateCounter(val)
-{
-    if (val == "up")
-    {
-        taskCounter = taskCounter + 1;
-    }
-    else if (val == 'down')
-    {
-        taskCounter = taskCounter - 1;
-    }
-    else if("clear")
-    {
-        taskCounter = 0;
-    }
-    console.log(taskCounter);
-
+    if (val == "up"){taskCounter = taskCounter + 1;}
+    else if (val == 'down'){taskCounter = taskCounter - 1;}
+    else if("clear"){taskCounter = 0;}
     counterSpan.textContent = taskCounter;
 }
 
-function editTask()
-{
-    console.log('edit');
-}
