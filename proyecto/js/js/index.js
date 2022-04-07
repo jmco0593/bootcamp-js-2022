f4311b4d96a3e9b3f4895b67b1b12b59b2b98399
jmco_0593 <jorgemario.cardonaoliva@gmail.com>
2022-04-07 12:21:03 -0600

    Finished JS part of tye proyect

--- proyecto/js/js/index.js
+++ proyecto/js/js/index.js
@@ -1,15 +1,21 @@
 
-//DEFINITIONS
+//ELEMENT DEFINITIONS
 const taskInput = document.getElementById("taskInput");
 const tasksList = document.getElementById("taskList");
 const completedTaskList = document.getElementById("completedTaskList");
 const addTaskButton = document.getElementById("addTaskButton");
 const clearTasksButton = document.getElementById("clearTasksButton");
-const counterSpan = document.getElementById("counterSpan");
+const tasksCounterSpan = document.getElementById("counterSpan");
+const counterCompletedSpan = document.getElementById("counterCompletedSpan");
 const body = document.getElementsByTagName("body")[0];
-var taskCounter = 0;
-var completedTaskCounter = 0;
+const completedTaskCard = document.getElementById("completedTaskCard");
 
+//GLOBAL VARS
+var taskId = 1;
+let allTasks = [];
+let allCompletedTasks = [];
+
+//EVENT LISTENERS
 body.addEventListener("keyup", function(event) {
     if (event.keyCode === 13) {
      event.preventDefault();
@@ -17,87 +23,20 @@ body.addEventListener("keyup", function(event) {
     }
   });
 
-taskInput.focus();
-
-
-function createTask(txt)
-{
-    const task = document.createElement("li");
-    var text = document.createTextNode(txt);
-    task.setAttribute('class', 'task');
-    task.setAttribute('onClick', 'editTask()');
-    task.appendChild(text);
-    return task;
-}
-
-function addTask()  
-{
+window.addEventListener('load', (event) => {
+    paintTasksUI(allTasks, tasksList);
     taskInput.focus();
-    if (taskInput.value)
-    {
-        const task = createTask(taskInput.value);
-        task.innerHTML = task.innerHTML + `
-            <span class="check" onclick="checkTask(event)"><i class="bi bi-check2-square"></i></span><span class="delete" onclick="removeTask(event)"><i class="bi bi-trash"></i></span>
-        `
-        tasksList.appendChild(task);
-        taskInput.value = "";
-        updateTaskCounter("up")
-    }
-    else
-    {
-        alert("Please enter some text in task input");
-    }
-}
+  });
 
-function checkTask(event)
+
+////////////////////////////////////////////////////UI
+function createli()
 {
-    taskInput.focus();
-    const anchor = event.currentTarget;
-    const li = anchor.parentElement;
-    const txt = li.textContent;
-    task = createTask(txt);
-
-    completedTaskList.appendChild(task);
-    removeTask(event);
-    updateTaskCounter("down");
+    const li = document.createElement("li");
+    li.setAttribute('class', 'task');
+    // li.appendChild(text);
+    return li;
 }
-
-function removeTask(event)
-{
-    console.log("remover");
-    const anchor = event.currentTarget;
-    const li = anchor.parentElement;
-    tasksList.removeChild(li);
-    taskInput.focus();  
-    updateTaskCounter("down");   
-}
-
-function clearList()
-{
-    if (taskCounter > 0)
-    {
-        if(confirm("Do you like to delete all your tasks?"))
-        {
-            removeChilds(tasksList);
-            taskInput.focus();
-            updateTaskCounter("clear");
-        }
-    }
-    else
-    {
-        alert("Please add some task first");
-    }
-}
-
-function updateTaskCounter(val)
-{
-    if (val == "up"){taskCounter = taskCounter + 1;}
-    else if (val == 'down'){taskCounter = taskCounter - 1;}
-    else if("clear"){taskCounter = 0;}
-    counterSpan.textContent = taskCounter;
-}
-
-
 
 function removeChilds(parent)
 {
@@ -108,12 +47,194 @@ function removeChilds(parent)
     
 }
 
-
-function updateCompletedTaskCounter(val)
+function completedTaskCardVisibility()
 {
-    if (val == "up"){taskCounter = taskCounter + 1;}
-    else if (val == 'down'){taskCounter = taskCounter - 1;}
-    else if("clear"){taskCounter = 0;}
-    counterSpan.textContent = taskCounter;
+    if (allCompletedTasks.length == 0)
+    {
+        completedTaskCard.style.visibility = "hidden";
+    }
+    else
+    {
+        completedTaskCard.style.visibility = "inherit";
+    }
 }
 
+///////////////////////////////////////TASK CARD
+function paintTasksUI(list, parent)
+{
+    let outsideNumber = 1;
+    for (element of list)
+    {
+        
+        const task = createli();
+        
+        task.innerHTML = task.innerHTML + `
+            <span class="taskId">${outsideNumber}</span>
+            <span id="taskId" class="taskId" style="visibility:hidden;">${element.id}</span>
+            <span id="taskText" class="taskText" onClick="editTask(event)">${element.task}</span>
+            <span class="check" onclick="addCompletedTask(event)">
+                <i class="bi bi-check2-square"></i>
+            </span>
+            <span class="delete" onclick="removeTask(event)">
+                <i class="bi bi-trash"></i>
+            </span>
+        `
+        parent.appendChild(task);
+        outsideNumber++;
+    }
+    //console.log(allTasks);
+}
+
+///////////////////////////////////////COMPLETED CARD
+function paintCompletedTasksUI(list, parent)
+{
+    console.log(allCompletedTasks);
+    let outsideNumber = 1;
+    for (element of list)
+    {
+        
+        const task = createli();
+        task.innerHTML = task.innerHTML + `
+            <span class="taskId">${outsideNumber}</span>
+            <span id="taskId" class="taskId" style="visibility:hidden;">${element.id}</span>
+            <span id="taskText" class="taskText">${element.task}</span>
+            <span class="delete" onclick="removeCompletedTask(event)">
+                <i class="bi bi-trash"></i>
+            </span>
+        `
+        parent.appendChild(task);
+        outsideNumber++;
+        
+    }
+    completedTaskCardVisibility();
+    
+}
+
+///////////////////////////////////////////////////////////////FUNCTIONALITY
+function updateCounter(counter, array)
+{
+    counter.textContent = array.length;
+}
+
+function selectTask(event, list)
+{
+    const anchor = event.currentTarget;
+    const li = anchor.parentElement;
+    const x = li.children;
+    const txtSpan = li.children[1].innerHTML;
+    let index = 0;
+    for (obj of list)
+    {
+        if (obj.id == txtSpan)
+        {
+            return index;
+        }
+        index++;
+    } 
+}
+
+/////////////////////////////////////TASK CARD
+function addTask()  
+{
+    if (taskInput.value)
+    {
+        const taskObject = {
+            id: taskId,
+            task: taskInput.value,
+        };
+        allTasks.push(taskObject);
+        removeChilds(tasksList);
+        paintTasksUI(allTasks, tasksList);
+        updateCounter(tasksCounterSpan, allTasks);
+
+        taskInput.value = "";
+        taskId = taskId + 1;
+    }
+    else
+    {
+        alert("Please enter some text in task input");
+    }
+    taskInput.focus();
+}
+
+function removeTask(event)
+{
+    const index = selectTask(event, allTasks);
+    allTasks.splice(index,1);
+    updateCounter(tasksCounterSpan, allTasks);
+    removeChilds(tasksList);
+    paintTasksUI(allTasks, tasksList);   
+    taskInput.focus(); 
+}
+
+function clearTaskList()
+{
+    if (allTasks.length > 0)
+    {
+        if(confirm("Do you like to delete all your tasks?"))
+        {
+            allTasks = [];
+            removeChilds(tasksList);
+            updateCounter(tasksCounterSpan, allTasks)
+            paintTasksUI(allTasks, tasksList);
+        }
+    }
+    else
+    {
+        alert("Please add some task first");
+    }
+}
+
+/////////////////////////////////////////////////COMPLETED CARD
+function addCompletedTask(event)  
+{
+    //ADDING TO COMPLETED TASKS
+    const index = selectTask(event, allTasks);
+    allCompletedTasks.push(allTasks[index]);
+    updateCounter(tasksCounterSpan, allTasks);
+    updateCounter(counterCompletedSpan, allCompletedTasks);
+    removeChilds(completedTaskList);
+    paintCompletedTasksUI(allCompletedTasks, completedTaskList);
+    removeTask(event);
+}
+
+function removeCompletedTask(event)
+{
+    const index = selectTask(event, allCompletedTasks);
+    allCompletedTasks.splice(index,1);
+    updateCounter(counterCompletedSpan, allCompletedTasks);
+    removeChilds(completedTaskList);
+    paintCompletedTasksUI(allCompletedTasks, completedTaskList);   
+    taskInput.focus(); 
+}
+
+function clearCompletedTaskList()
+{
+    if (allCompletedTasks.length > 0)
+    {
+        if(confirm("Do you like to delete all your completed tasks?"))
+        {
+            allCompletedTasks = [];
+            removeChilds(completedTaskList);
+            updateCounter(counterCompletedSpan, allCompletedTasks);
+            paintCompletedTasksUI(allCompletedTasks, completedTaskList);
+        }
+    }
+    else
+    {
+        alert("Please add some completed task first");
+    }
+}
+
+function editTask(event)
+{
+    const index = selectTask(event, allTasks);
+    const actualText = allTasks[index].task;
+    const newText = prompt("Enter the text that will replace the task that you selected", actualText);
+    if (!(newText == null || newText == ""))
+    {
+        allTasks[index].task = newText; 
+    }
+    removeChilds(tasksList);
+    paintTasksUI(allTasks, tasksList);
+}
