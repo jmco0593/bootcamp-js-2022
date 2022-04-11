370257579c2486e4874cae53118f5cc0cf9a1804
jmco_0593 <jorgemario.cardonaoliva@gmail.com>
2022-04-11 17:35:19 -0600

    Hasta clase 25

--- clases/react/clase18-configurando_webpack_con_react/servidor/index.js
+++ clases/react/clase18-configurando_webpack_con_react/servidor/index.js
@@ -1,12 +1,21 @@
 import express from "express";
+import cors from "cors";
 import bodyParser from "body-parser";
 
-let lastId=0;
-const productos = []
+let lastId = 1;
+let productos = [
+    {
+        nombre: "producto a",
+        cantidad: 1,
+        precio: 10,
+        codigo: lastId
+    }
+]
 
 //DECLARACIÓN DE OBJETO EXPRESS
 const app = express();
 
+app.use(cors());
 //Método que me sirve para parsear objeto json
 app.use(bodyParser.json({type:'application/json'}));
 app.use(logs);
@@ -16,7 +25,21 @@ app.get("/", (req, res) => res.send("<h1>Hola mundo </h1>"));
 //Forma con middleware
 //app.get("/productos", isAuthenticated, (req, res) => res.json(productos));
 //Forma sin middleware
-app.get("/productos", (req, res) => res.json(productos));
+//app.get("/productos", (req, res) => res.json(productos));
+app.get("/productos", (req, res) => {
+
+    const filtro = req.query.filtro;
+
+    if (filtro)
+    {
+        res.json(productos.filter(p => p.nombre.indexOf(filtro) >= 0));
+    }
+    else
+    {
+        res.json(productos);
+    }
+    res.json(productos);
+});
 
 //use sirve para que ejecute la función en paréntesis cada vez que se ejecuta la aplicación
 //app.use(isAuthenticated);
@@ -31,8 +54,70 @@ app.post("/productos", (req, res) => {
     res.json(producto);
 })
 
-app.listen(5000, () => {
-    console.log("Servidor express corriendo en puerto 5000");
+//MODIFICA UN ELEMENT EXISTENTE, PUT DE HTTP
+app.put("/productos/:codigo", (req, res) =>{
+    const codigo = parseInt(req.params.codigo, 10);
+    const producto = productos.find(p => p.codigo == codigo);
+
+    if (!producto)
+    {
+        res.status(404);
+        res.json({
+            mensaje: "No existe ningún producto con codigo " + codigo
+        })
+    }
+    else
+    {
+        const index = productos.indexOf(producto);
+        const nuevoProducto = productos[index] = {...req.body, codigo};
+        res.status(200);
+        res.json(nuevoProducto);
+    }
+})
+//PARA SOLICITAR INFOR PARA PRODUCTO UNICO
+app.get("/productos/:codigo", (req, res) =>{
+    const codigo = parseInt(req.params.codigo, 10);
+    const producto = productos.find(p => p.codigo == codigo);
+
+    if (!producto)
+    {
+        res.status(404);
+        res.json({
+            mensaje: "No existe ningún producto con codigo " + codigo
+        });
+    }
+    else
+    {
+        res.status(200);
+        res.json(producto);
+    }
+})
+
+//ELIMINA UN ELEMENT EXISTENTE, delete DE HTTP
+app.delete("/productos/:codigo", (req, res) =>{
+    const codigo = parseInt(req.params.codigo, 10);
+    const producto = productos.find(p => p.codigo == codigo);
+
+    if (!producto)
+    {
+        res.status(404);
+        res.json({
+            mensaje: "No existe ningún producto con codigo " + codigo
+        });
+    }
+    else
+    {
+        productos = productos.filter(x => x != producto);
+        res.status(200);
+        res.json({message: "Producto eliminado"});
+    }
+})
+
+
+
+
+app.listen(5001, () => {
+    console.log("Servidor express corriendo en puerto 5001");
 })
 
 // function isAuthenticated(req, res, next) {
